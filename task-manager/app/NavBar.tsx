f4719b462e097e2c88bf0ccb0ff79e39c0c624c0
Xiao Liang <thomasliangus@gmail.com>
2024-09-15 15:52:30 -0700

    Style the active link in the navbar

--- task-manager/app/NavBar.tsx
+++ task-manager/app/NavBar.tsx
@@ -1,12 +1,16 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { SiGoogletasks } from "react-icons/si";
+import classnames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Task", href: "/task" },
+    { label: "Tasks", href: "/task" },
   ];
+  const currentPathname = usePathname();
   return (
     <nav className="flex mb-5 space-x-8 border-b px-5 h-14 items-center">
       <Link href="/">
@@ -14,13 +18,18 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-8">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors "
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                "text-zinc-900": link.href === currentPathname,
+                "text-zinc-500": link.href !== currentPathname,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
