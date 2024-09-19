178d5d50e1a20f4ffff1091dfb24662a6c55f4dd
Xiao Liang <thomasliangus@gmail.com>
2024-09-19 16:51:14 -0700

    Implement Client Side Validation

--- task-manager/app/Tasks/new/page.tsx
+++ task-manager/app/Tasks/new/page.tsx
@@ -1,19 +1,26 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import createTaskSchema from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface TaskForm {
-  title: string;
-  description: string;
-}
+type TaskForm = z.infer<typeof createTaskSchema>;
 const NewTaskPage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<TaskForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TaskForm>({
+    resolver: zodResolver(createTaskSchema),
+  });
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl">
@@ -34,6 +41,11 @@ const NewTaskPage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -41,7 +53,11 @@ const NewTaskPage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit a new task</Button>
       </form>
     </div>
