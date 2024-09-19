25ab5f476402b3460dbaef3b88981bd912897484
Xiao Liang <thomasliangus@gmail.com>
2024-09-19 15:24:25 -0700

    Handle Errors

--- task-manager/app/Tasks/new/page.tsx
+++ task-manager/app/Tasks/new/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface TaskForm {
   title: string;
@@ -13,25 +14,37 @@ interface TaskForm {
 const NewTaskPage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<TaskForm>();
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/tasks", data);
-        router.push("/Tasks");
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/tasks", data);
+            router.push("/Tasks");
+          } catch (error) {
+            setError("An Unexpected Error Occured");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <Button>Submit a new task</Button>
-    </form>
+        <Button>Submit a new task</Button>
+      </form>
+    </div>
   );
 };
 
