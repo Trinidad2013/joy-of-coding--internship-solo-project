618f5938d8adfb0c089bc5e71f412c7b187acbe1
Xiao Liang <thomasliangus@gmail.com>
2024-09-19 20:05:52 -0700

    Refactor: Move inline function

--- task-manager/app/Tasks/new/page.tsx
+++ task-manager/app/Tasks/new/page.tsx
@@ -25,6 +25,18 @@ const NewTaskPage = () => {
   });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/tasks", data);
+      router.push("/Tasks");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An Unexpected Error Occured");
+    }
+  });
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -32,19 +44,7 @@ const NewTaskPage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/tasks", data);
-            router.push("/Tasks");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An Unexpected Error Occured");
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root placeholder="Title" {...register("title")} />
 
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
