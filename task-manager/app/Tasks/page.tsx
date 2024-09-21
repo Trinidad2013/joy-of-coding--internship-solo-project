ea4cb18ca80dde1d2565906cd76b6e33ef09225f
Xiao Liang <thomasliangus@gmail.com>
2024-09-21 16:23:02 -0700

    Show the tasks

--- task-manager/app/Tasks/page.tsx
+++ task-manager/app/Tasks/page.tsx
@@ -1,13 +1,46 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const TasksPage = () => {
+const TasksPage = async () => {
+  const tasks = await prisma.task.findMany();
   return (
     <div className="bg-white text-black">
-      <Button>
-        <Link href="/Tasks/new">New Task</Link>
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/Tasks/new">New Task</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Task</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {tasks.map((task) => (
+            <Table.Row key={task.id}>
+              <Table.Cell>
+                {task.title}
+                <div className="block md:hidden">{task.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {task.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {task.createAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
