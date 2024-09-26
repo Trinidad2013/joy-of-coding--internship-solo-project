dd0dccc41e494de2503e066e2a83cde8e941d12b
Xiao Liang <thomasliangus@gmail.com>
2024-09-26 11:02:26 -0700

    Show a confirmation dialog box

--- task-manager/app/Tasks/[id]/DeleteTaskButton.tsx
+++ task-manager/app/Tasks/[id]/DeleteTaskButton.tsx
@@ -1,8 +1,36 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const DeleteTaskButton = ({ taskId }: { taskId: number }) => {
-  return <Button color="red">Delete this Task</Button>;
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete this Task</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? Once you delete this task, it can not be restore.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Delete this task
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  );
 };
 
 export default DeleteTaskButton;
